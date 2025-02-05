6e8a4a6f4ce253dae918be781c4d6f336999fc73
Dmitri Smirnov <dmixsmi@gmail.com>
2025-02-05 18:40:58 -0500

    refactor: run opticks test from a script (#32)

---
+++ tests/test_opticks.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+QCurandStateMonolithic_SPEC=1:0:0  $OPTICKS_PREFIX/lib/QCurandStateMonolithicTest
+QCurandStateMonolithic_SPEC=3:0:0  $OPTICKS_PREFIX/lib/QCurandStateMonolithicTest
+QCurandStateMonolithic_SPEC=10:0:0 $OPTICKS_PREFIX/lib/QCurandStateMonolithicTest
+
+mv $HOME/.opticks/rngcache/RNG/QCurandStateMonolithic_1M_0_0.bin $HOME/.opticks/rngcache/RNG/QCurandState_1000000_0_0.bin
+mv $HOME/.opticks/rngcache/RNG/QCurandStateMonolithic_3M_0_0.bin $HOME/.opticks/rngcache/RNG/QCurandState_3000000_0_0.bin
+mv $HOME/.opticks/rngcache/RNG/QCurandStateMonolithic_10M_0_0.bin $HOME/.opticks/rngcache/RNG/QCurandState_10000000_0_0.bin
+
+install -D $OPTICKS_HOME/tests/GEOM.sh $HOME/.opticks/GEOM/
+
+ctest --test-dir $OPTICKS_PREFIX/build/okconf
+ctest --test-dir $OPTICKS_PREFIX/build/sysrap -E STTFTest
+ctest --test-dir $OPTICKS_PREFIX/build/ana
+ctest --test-dir $OPTICKS_PREFIX/build/analytic
+ctest --test-dir $OPTICKS_PREFIX/build/bin
+ctest --test-dir $OPTICKS_PREFIX/build/CSG -E "CSGNodeTest|CSGPrimSpecTest|CSGPrimTest|CSGFoundryTest|CSGFoundry_getCenterExtent_Test|CSGFoundry_findSolidIdx_Test|CSGNameTest|CSGTargetTest|CSGTargetGlobalTest|CSGFoundry_MakeCenterExtentGensteps_Test|CSGFoundry_getFrame_Test|CSGFoundry_getFrameE_Test|CSGFoundry_getMeshName_Test|CSGFoundryLoadTest|CSGQueryTest|CSGSimtraceTest|CSGSimtraceRerunTest|CSGSimtraceSampleTest|CSGCopyTest|CSGScanTest"
+ctest --test-dir $OPTICKS_PREFIX/build/qudarap -E "QSimTest|QOpticalTest|QEvent_Lifecycle_Test|QSim_Lifecycle_Test|QSimWithEventTest"
+ctest --test-dir $OPTICKS_PREFIX/build/CSGOptiX -E CSGOptiXRenderTest
+ctest --test-dir $OPTICKS_PREFIX/build/gdxml
+ctest --test-dir $OPTICKS_PREFIX/build/u4 -E "U4GDMLReadTest|U4RandomTest|U4TraverseTest"
+ctest --test-dir $OPTICKS_PREFIX/build/g4cx -E G4CXRenderTest
