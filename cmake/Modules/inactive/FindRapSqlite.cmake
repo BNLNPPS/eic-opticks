884203b4a4da0c7dc74118b051a2856632206e38
Dmitri Smirnov <dmixsmi@gmail.com>
2025-03-04 18:47:35 -0500

    chore(cmake): remove unused files (#48)

--- cmake/Modules/inactive/FindRapSqlite.cmake
+++
@@ -1,3 +0,0 @@
-set(RapSqlite_LIBRARIES "$ENV{LOCAL_BASE}/env/sqlite/rapsqlite/lib/libRapSqlite.dylib")
-set(RapSqlite_INCLUDE_DIRS "$ENV{LOCAL_BASE}/env/sqlite/rapsqlite/include")
-set(RapSqlite_DEFINITIONS "")
--- cmake/Modules/old/FindGLEQ.cmake
+++
@@ -1,7 +0,0 @@
-
-set(GLEQ_PREFIX "${OPTICKS_PREFIX}/externals/gleq")
-
-set(GLEQ_LIBRARIES    "" )
-set(GLEQ_INCLUDE_DIRS "${GLEQ_PREFIX}")
-set(GLEQ_DEFINITIONS "")
-
