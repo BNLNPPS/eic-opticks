7e8d64205d98a5c6ca6bad8ff4301dc85a43716f
Dmitri Smirnov <dmixsmi@gmail.com>
2025-03-04 17:12:05 -0500

    test: run in parallel

--- tests/test_opticks.sh
+++ tests/test_opticks.sh
@@ -15,14 +15,14 @@ install -D $OPTICKS_HOME/tests/GEOM.sh $HOME/.opticks/GEOM/GEOM.sh
 DTYPE=np.float32 $OPTICKS_HOME/ana/input_photons.py
 DTYPE=np.float64 $OPTICKS_HOME/ana/input_photons.py
 
-ctest --test-dir $OPTICKS_PREFIX/build/okconf
-ctest --test-dir $OPTICKS_PREFIX/build/sysrap -E STTFTest
-ctest --test-dir $OPTICKS_PREFIX/build/ana
-ctest --test-dir $OPTICKS_PREFIX/build/analytic
-ctest --test-dir $OPTICKS_PREFIX/build/bin
-ctest --test-dir $OPTICKS_PREFIX/build/CSG -E "CSGNodeTest|CSGPrimSpecTest|CSGPrimTest|CSGFoundryTest|CSGFoundry_getCenterExtent_Test|CSGFoundry_findSolidIdx_Test|CSGNameTest|CSGTargetTest|CSGTargetGlobalTest|CSGFoundry_MakeCenterExtentGensteps_Test|CSGFoundry_getFrame_Test|CSGFoundry_getFrameE_Test|CSGFoundry_getMeshName_Test|CSGFoundryLoadTest|CSGQueryTest|CSGSimtraceTest|CSGSimtraceRerunTest|CSGSimtraceSampleTest|CSGCopyTest|CSGScanTest"
-ctest --test-dir $OPTICKS_PREFIX/build/qudarap -E "QSimTest|QOpticalTest|QEvent_Lifecycle_Test|QSim_Lifecycle_Test|QSimWithEventTest"
-ctest --test-dir $OPTICKS_PREFIX/build/CSGOptiX -E CSGOptiXRenderTest
-ctest --test-dir $OPTICKS_PREFIX/build/gdxml
-ctest --test-dir $OPTICKS_PREFIX/build/u4 -E "U4GDMLReadTest|U4RandomTest|U4TraverseTest"
-ctest --test-dir $OPTICKS_PREFIX/build/g4cx -E G4CXRenderTest
+ctest --test-dir $OPTICKS_PREFIX/build/okconf -j
+ctest --test-dir $OPTICKS_PREFIX/build/sysrap -j -E STTFTest
+ctest --test-dir $OPTICKS_PREFIX/build/ana -j
+ctest --test-dir $OPTICKS_PREFIX/build/analytic -j
+ctest --test-dir $OPTICKS_PREFIX/build/bin -j
+ctest --test-dir $OPTICKS_PREFIX/build/CSG -j -E "CSGNodeTest|CSGPrimSpecTest|CSGPrimTest|CSGFoundryTest|CSGFoundry_getCenterExtent_Test|CSGFoundry_findSolidIdx_Test|CSGNameTest|CSGTargetTest|CSGTargetGlobalTest|CSGFoundry_MakeCenterExtentGensteps_Test|CSGFoundry_getFrame_Test|CSGFoundry_getFrameE_Test|CSGFoundry_getMeshName_Test|CSGFoundryLoadTest|CSGQueryTest|CSGSimtraceTest|CSGSimtraceRerunTest|CSGSimtraceSampleTest|CSGCopyTest|CSGScanTest"
+ctest --test-dir $OPTICKS_PREFIX/build/qudarap -j -E "QSimTest|QOpticalTest|QEvent_Lifecycle_Test|QSim_Lifecycle_Test|QSimWithEventTest"
+ctest --test-dir $OPTICKS_PREFIX/build/CSGOptiX -j -E CSGOptiXRenderTest
+ctest --test-dir $OPTICKS_PREFIX/build/gdxml -j
+ctest --test-dir $OPTICKS_PREFIX/build/u4 -j -E "U4GDMLReadTest|U4RandomTest|U4TraverseTest"
+ctest --test-dir $OPTICKS_PREFIX/build/g4cx -j -E G4CXRenderTest
