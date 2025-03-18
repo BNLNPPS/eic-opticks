9dc0ac19193d80557df9e017626e916491d47cde
Dmitri Smirnov <dmixsmi@gmail.com>
2025-03-17 20:52:48 -0400

    test: re-enable many CSG tests

--- tests/test_opticks.sh
+++ tests/test_opticks.sh
@@ -15,14 +15,17 @@ install -D $OPTICKS_HOME/tests/GEOM.sh $HOME/.opticks/GEOM/GEOM.sh
 DTYPE=np.float32 $OPTICKS_HOME/ana/input_photons.py
 DTYPE=np.float64 $OPTICKS_HOME/ana/input_photons.py
 
+export GEOM=RaindropRockAirWater
+export G4CXOpticks__setGeometry_saveGeometry=$TMP/G4CXOpticks_setGeometry_Test/$GEOM
+
 ctest --test-dir $OPTICKS_PREFIX/build/okconf -j
 ctest --test-dir $OPTICKS_PREFIX/build/sysrap -j -E STTFTest
 ctest --test-dir $OPTICKS_PREFIX/build/ana -j
 ctest --test-dir $OPTICKS_PREFIX/build/analytic -j
 ctest --test-dir $OPTICKS_PREFIX/build/bin -j
-ctest --test-dir $OPTICKS_PREFIX/build/CSG -j -E "CSGNodeTest|CSGPrimSpecTest|CSGPrimTest|CSGFoundryTest|CSGFoundry_getCenterExtent_Test|CSGFoundry_findSolidIdx_Test|CSGNameTest|CSGTargetTest|CSGTargetGlobalTest|CSGFoundry_MakeCenterExtentGensteps_Test|CSGFoundry_getFrame_Test|CSGFoundry_getFrameE_Test|CSGFoundry_getMeshName_Test|CSGFoundryLoadTest|CSGQueryTest|CSGSimtraceTest|CSGSimtraceRerunTest|CSGSimtraceSampleTest|CSGCopyTest|CSGScanTest"
+ctest --test-dir $OPTICKS_PREFIX/build/g4cx -j -E G4CXRenderTest
+ctest --test-dir $OPTICKS_PREFIX/build/CSG -j -E CSGQueryTest
 ctest --test-dir $OPTICKS_PREFIX/build/qudarap -j -E "QSimTest|QOpticalTest|QEvent_Lifecycle_Test|QSim_Lifecycle_Test|QSimWithEventTest"
 ctest --test-dir $OPTICKS_PREFIX/build/CSGOptiX -j -E CSGOptiXRenderTest
 ctest --test-dir $OPTICKS_PREFIX/build/gdxml -j
 ctest --test-dir $OPTICKS_PREFIX/build/u4 -j -E "U4GDMLReadTest|U4RandomTest|U4TraverseTest"
-ctest --test-dir $OPTICKS_PREFIX/build/g4cx -j -E G4CXRenderTest
