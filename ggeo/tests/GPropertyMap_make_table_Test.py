19183327cb0a0039228160fee3ce7cf6d5d97018
Dmitri Smirnov <dmixsmi@gmail.com>
2025-03-28 10:19:07 -0400

    chore: rm ggeo

--- ggeo/tests/GPropertyMap_make_table_Test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import os, numpy as np
-from opticks.ana.fold import Fold
-
-if __name__ == '__main__':
-    f = Fold.Load("$GPropertyMap_BASE", "LS", symbol="f", order="ascend" )
-    print(repr(f))
-
