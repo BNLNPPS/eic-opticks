621424243bceb06e6bf0f3364ae849ca115e3aaa
Dmitri Smirnov <dmixsmi@gmail.com>
2025-07-08 19:53:53 -0400

    test(sysrap): add overlooked test for visible devices (#122)

--- sysrap/tests/sdevice_test.cc
+++ sysrap/tests/sdevice_test.cc
@@ -11,10 +11,22 @@ See notes in sdevice_test.sh
 
 int main(int argc, char** argv)
 {  
-    std::vector<sdevice> devs ; 
-    sdevice::Visible(devs, "$HOME/.opticks/runcache", false );  
-    std::cout << sdevice::Desc( devs ) ; 
-    //std::cout << sdevice::Brief( devs ) ; 
+    std::vector<sdevice> devs;
 
-    return 0 ; 
+    bool nosave = true;
+    sdevice::Visible(devs, nullptr, nosave);
+
+    std::cout << sdevice::Desc( devs )
+              << sdevice::Brief( devs ) << '\n'
+              << sdevice::VRAM( devs );
+
+    std::vector<sdevice> devs2;
+    sdevice::Load(devs2, "./");
+
+    std::cout << devs2.size() << '\n'
+              << sdevice::Desc( devs2 )
+              << sdevice::Brief( devs2 ) << '\n'
+              << sdevice::VRAM( devs2 );
+
+    return 0;
 }
