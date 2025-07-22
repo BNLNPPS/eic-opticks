BNLNPPS/eic-opticks

commit e22adee6f70aeb6856ff2e77857d8fc931b90224

ci: build image and push to ghcr and docker hub registries (#131)

.github/workflows/build-push.yaml
@@ -1,0 +1,66 @@
+name: Build Push Image
+
+on:
+  push:
+    branches:
+      - main
+      - test
+    tags:
+      - '*'
+
+jobs:
+
+  build-push:
+    runs-on: [gpu]
+
+    strategy:
+      matrix:
+        target: [release, develop]
+
+    steps:
+      - name: Define environment variables
+        run: |
+          echo IMAGE_NAME=$(echo ${{ github.repository }} | tr '[:upper:]' '[:lower:]') >> $GITHUB_ENV
+          echo IMAGE_TAG=$(echo ${{ matrix.target }}) >> $GITHUB_ENV
+          if [ "${{ matrix.target }}" = "release" ]; then
+            echo IMAGE_TAG=latest >> $GITHUB_ENV
+          fi
+
+      - name: Checkout code
+        uses: actions/checkout@v4
+
+      - name: Set up Docker Buildx
+        uses: docker/setup-buildx-action@v3
+
+      - name: Log in to GHCR
+        uses: docker/login-action@v3
+        with:
+          registry: ghcr.io
+          username: ${{ github.actor }}
+          password: ${{ secrets.GITHUB_TOKEN }}
+
+      - name: Log in to Docker Hub
+        uses: docker/login-action@v3
+        with:
+          username: ${{ secrets.DOCKERHUB_USERNAME }}
+          password: ${{ secrets.DOCKERHUB_TOKEN }}
+
+      - name: Build and push to registries
+        uses: docker/build-push-action@v6
+        with:
+          push: true
+          tags: |
+           ghcr.io/${{ env.IMAGE_NAME }}:${{ env.IMAGE_TAG }}
+           docker.io/${{ env.IMAGE_NAME }}:${{ env.IMAGE_TAG }}
+          target: ${{ matrix.target }}
+          cache-from: type=local,src=/home/runner/.buildx-cache
+          cache-to: type=local,dest=/home/runner/.buildx-cache,mode=max
+
+  build-push-cleanup:
+    runs-on: ubuntu-latest
+    needs: build-push
+    steps:
+      - name: Checkout code
+        uses: actions/checkout@v4
+
+      - run: .github/workflows/delete_untagged.py ${{ secrets.GITHUB_TOKEN}}

.github/workflows/delete_untagged.py
@@ -1,0 +1,25 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import requests
+import types
+
+parser = argparse.ArgumentParser()
+parser.add_argument("token", help="GitHub token")
+args = parser.parse_args()
+
+domain = "api.github.com"
+org = "bnlnpps"
+package_type = "container"
+package_name = "eic-opticks"
+
+api_url = f"https://{domain}/orgs/{org}/packages/{package_type}/{package_name}/versions"
+
+respjson = requests.get(api_url, auth=("token", args.token))
+entries  = json.loads(respjson.text, object_hook=lambda d: types.SimpleNamespace(**d))
+
+for e in entries:
+    if not e.metadata.container.tags:
+        response = requests.delete(api_url + f"/{e.id}", auth=("token", args.token))
+        print("delete", e.id, e.html_url, e.name, response.url, response.status_code)

commit 647b08281e04c6e7af1b071ab29bb1611b148cc5

fix: push to ghcr and docker hub registries (#134)
This PR addresses an issue introduced in
https://github.com/BNLNPPS/eic-opticks/pull/131

Similar to https://github.com/actions/delete-package-versions/issues/90
and
https://github.com/actions/delete-package-versions/issues/162

.github/workflows/build-push.yaml
@@ -60,7 +60,4 @@ jobs:
     runs-on: ubuntu-latest
     needs: build-push
     steps:
-      - name: Checkout code
-        uses: actions/checkout@v4
-
-      - run: .github/workflows/delete_untagged.py ${{ secrets.GITHUB_TOKEN}}
+      - uses: dataaxiom/ghcr-cleanup-action@v1

.github/workflows/delete_untagged.py
@@ -1,25 +1,0 @@
-#!/usr/bin/env python3
-
-import argparse
-import json
-import requests
-import types
-
-parser = argparse.ArgumentParser()
-parser.add_argument("token", help="GitHub token")
-args = parser.parse_args()
-
-domain = "api.github.com"
-org = "bnlnpps"
-package_type = "container"
-package_name = "eic-opticks"
-
-api_url = f"https://{domain}/orgs/{org}/packages/{package_type}/{package_name}/versions"
-
-respjson = requests.get(api_url, auth=("token", args.token))
-entries  = json.loads(respjson.text, object_hook=lambda d: types.SimpleNamespace(**d))
-
-for e in entries:
-    if not e.metadata.container.tags:
-        response = requests.delete(api_url + f"/{e.id}", auth=("token", args.token))
-        print("delete", e.id, e.html_url, e.name, response.url, response.status_code)