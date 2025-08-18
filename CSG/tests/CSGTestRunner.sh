8b7e459fadec7417ff95ab6e9bee75ac29575862
Dmitri Smirnov <dmixsmi@gmail.com>
2025-08-18 17:39:19 -0400

    reject buggy change

--- CSG/tests/CSGTestRunner.sh
+++ CSG/tests/CSGTestRunner.sh
@@ -43,11 +43,7 @@ Resolve_CFBaseFromGEOM()
     fi  
 }
 
-if [ -n "$GEOM" -a -n "${GEOM}_CFBaseFromGEOM" ]; then
-   echo $BASH_SOURCE - using external config for GEOM $GEOM ${GEOM}_CFBaseFromGEOM 
-else
-   Resolve_CFBaseFromGEOM
-fi
+Resolve_CFBaseFromGEOM
 
 
 
