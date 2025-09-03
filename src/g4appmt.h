bc009f9becf565a285c069afd7cea36a4e51cf0b
Dmitri Smirnov <dmixsmi@gmail.com>
2025-09-03 11:06:06 -0400

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/g4appmt.h
+++ src/g4appmt.h
@@ -122,8 +122,7 @@ struct PhotonHit : public G4VHit
     // Print method
     void Print() override
     {
-        G4cout << "Detector id: " << fid << " energy: " << fenergy << " nm"
-               << " time: " << ftime << " ns"
+        G4cout << "Detector id: " << fid << " energy: " << fenergy << " nm" << " time: " << ftime << " ns"
                << " position: " << fposition << " direction: " << fdirection << " polarization: " << fpolarization
                << G4endl;
     }
