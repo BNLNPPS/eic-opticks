54f93db52f0709027cdafa6fc251abf08df1d03a
Dmitri Smirnov <dmixsmi@gmail.com>
2025-10-13 10:59:59 -0400

    Revert "test: update reference for gpu vs cpu diff results"
    This reverts commit 7b99d5d3b7a04bf97b5b3209c4e5a5d313db5112.

--- tests/compare_ab.py
+++ tests/compare_ab.py
@@ -11,4 +11,4 @@ assert a.shape == b.shape
 diff = [i for i, (a, b) in enumerate(zip(a[:, 1:], b[:, 0:-1])) if not np.allclose(a, b, rtol=0, atol=1e-5)]
 print(diff)
 
-assert diff == [14, 22, 32, 34, 40, 81, 85]
+assert diff == [6, 14, 22, 40, 50, 81, 91]
