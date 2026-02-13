02d232b9ed598b3e94164469dff1368d2cf472ed
Dmitri Smirnov <dmixsmi@gmail.com>
2026-02-13 13:22:43 -0500

    Apply suggestion from @plexoos

--- sysrap/scuda.h
+++ sysrap/scuda.h
@@ -127,7 +127,6 @@ SUTIL_INLINE SUTIL_HOSTDEVICE unsigned long long min(unsigned long long a, unsig
 
 namespace sysrap
 { // avoid name conflict with std::lerp function
-{ // avoid name conflict with std::lerp function
 
 /** lerp */
 SUTIL_INLINE SUTIL_HOSTDEVICE float lerp(const float a, const float b, const float t)
